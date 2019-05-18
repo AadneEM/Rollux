a810e2bc3d3ca698ea947fb229147f1734897198
Andreas Larsen <andreas@northcode.no>
2019-05-18 15:09:05 +0200

    Fix auth.json loading from working directory
    Added missing semicolons from linter warnings

--- bot.js
+++ bot.js
@@ -1,7 +1,9 @@
 var Discord = require('discord.io');
 var logger = require('winston');
-var auth = require('./auth.json');
-var diceRoller = require('./diceRolling.js')
+var fs = require('fs');
+var diceRoller = require('./diceRolling.js');
+
+var auth = JSON.parse(fs.readFileSync('./auth.json', 'utf8'));
 
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
@@ -26,17 +28,17 @@ bot.on('message', function (user, userID, channelID, message, evt) {
             var args = message.substring(1).split(' ');
             var cmd = args[0];
 
-            console.log(message)
+            console.log(message);
 
             switch (cmd) {
                 case 'hi':
-                    sendMessage(channelID, 'Hello, World!')
+                sendMessage(channelID, 'Hello, World!');
                 case 'roll':
                     if (/([+\-]?\d{0,}d\d{1,})([+\-*x/]\d{1,}){0,}/gi.test(message.substring(6).replace(/\s/g, ''))) {
                         sendMessage(
                             channelID,
                             message.substring(6) + ': ' + diceRoller.roll(message.substring(6))
-                        )
+                        );
                     } else {
                         sendMessage(channelID, "I don't recognize this: \""
                             + message
@@ -48,17 +50,17 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                     break;
             }
         } catch (error) {
-            console.log(error)
+            console.log(error);
         }
     }
 });
 
 function sendMessage(client, message) {
     if (message.length > 2500) {
-        message = 'Response was to long. Sorry'
+        message = 'Response was to long. Sorry';
     }
     bot.sendMessage({
         to: client,
         message: message
-    })
+    });
 }
